d5f681cf1516654a39781c059531630a814fd25d
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-09-28 18:19:17 -0400

    Created express server for site.

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = required('express');
+var server = express();
+
+server.use(express.static(_dirname + "/public"));
+
+server.get('/', function(request, response) {
+  response.sendFile('public/index.html', {root: __dirname});
+});
+
+server.get('/bparty', function(request, response) {
+  response.sendFile('public/bparty.html', {root: __dirname});
+});
+
+server.get('/bride', function(request, response) {
+  response.sendFile('public/bride.html', {root: __dirname});
+});
+
+server.get('/groom', function(request, response) {
+  response.sendFile('public/groom.html', {root: __dirname});
+});
+
+server.get('/macbk', function(request, response) {
+  response.sendFile('public/macbk.html', {root: __dirname});
+});
+
+server.get('/met', function(request, response) {
+  response.sendFile('public/met.html', {root: __dirname});
+});
+
+server.get('/wedding', function(request, response) {
+  response.sendFile('public/wedding.html', {root: __dirname});
+});
+
+server.listen(8080, function() {
+  console.log('Now listening on port...', 8080);
+});
