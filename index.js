0b6539820031a141fd40bbc07c2a6730c215448c
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-09-29 18:14:07 -0400

    Made corrections to express server code.

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ server.get('/', function(request, response) {
   response.sendFile('public/index.html', {root: __dirname});
 });
 
-server.get('/bparty', function(request, response) {
+server.get('bparty.html', function(request, response) {
   response.sendFile('public/bparty.html', {root: __dirname});
 });
 
-server.get('/bride', function(request, response) {
+server.get('bride.html', function(request, response) {
   response.sendFile('public/bride.html', {root: __dirname});
 });
 
-server.get('/groom', function(request, response) {
+server.get('groom.html', function(request, response) {
   response.sendFile('public/groom.html', {root: __dirname});
 });
 
-server.get('/macbk', function(request, response) {
+server.get('macbk.html', function(request, response) {
   response.sendFile('public/macbk.html', {root: __dirname});
 });
 
-server.get('/met', function(request, response) {
+server.get('met.html', function(request, response) {
   response.sendFile('public/met.html', {root: __dirname});
 });
 
-server.get('/wedding', function(request, response) {
+server.get('wedding.html', function(request, response) {
   response.sendFile('public/wedding.html', {root: __dirname});
 });
 
