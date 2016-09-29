3136a397404c97035bc3e6669e24be47b019d516
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-09-29 18:19:59 -0400

    Fixed an error

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 var express = require('express');
 var server = express();
 
+var port = process.env.PORT || 8080;
+
 server.use(express.static(__dirname + "/public"));
 
 server.get('/', function(request, response) {
   response.sendFile('public/index.html', {root: __dirname});
 });
 
-server.get('bparty.html', function(request, response) {
+server.get('/bparty', function(request, response) {
   response.sendFile('public/bparty.html', {root: __dirname});
 });
 
-server.get('bride.html', function(request, response) {
+server.get('/bride', function(request, response) {
   response.sendFile('public/bride.html', {root: __dirname});
 });
 
-server.get('groom.html', function(request, response) {
+server.get('/groom', function(request, response) {
   response.sendFile('public/groom.html', {root: __dirname});
 });
 
-server.get('macbk.html', function(request, response) {
+server.get('/macbk', function(request, response) {
   response.sendFile('public/macbk.html', {root: __dirname});
 });
 
-server.get('met.html', function(request, response) {
+server.get('/met', function(request, response) {
   response.sendFile('public/met.html', {root: __dirname});
 });
 
-server.get('wedding.html', function(request, response) {
+server.get('/wedding', function(request, response) {
   response.sendFile('public/wedding.html', {root: __dirname});
 });
 
